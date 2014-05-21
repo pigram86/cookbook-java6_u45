82538fc42f35eed6442f6b8e5970ba74f0828f6c
pigram86 <todd@toddpigram.com>
2014-05-21 12:57:16 -0400

    initial config

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of java6_u45
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'java6_u45'
+maintainer       'Todd Pigram'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
+description      'Installs/Configures java6_u45'
+long_description 'Installs/Configures java6_u45'
+version          '0.3.0'
+depends          "windows"
+
